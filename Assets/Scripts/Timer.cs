4cbb0268f76aedba9bde6cc6d045ce9e38378a63
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-11-16 15:46:21 -0500

    Cambios
    Ola

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float tiempoMaximo;
+
+    [SerializeField] private Slider slider;
+
+    private float tiempoActual;
+
+    private bool tiempoActivado = false;
+
+
+    private void Start()
+    {
+        ActivarTemporizador();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (tiempoActivado)
+        {
+            CambiarContador();
+        }
+    }
+
+    private void CambiarContador()
+    {
+        tiempoActual -= Time.deltaTime;
+
+        if (tiempoActual >= 0)
+        {
+            slider.value = tiempoActual;
+        }
+
+
+        if (tiempoActual <= 0)
+        {
+            Debug.Log("Derrota");
+            CambiarTemporizador(false);
+        }
+    }
+
+    private void CambiarTemporizador(bool estado)
+    {
+        tiempoActivado = estado;
+    }
+
+    public void ActivarTemporizador()
+    {
+        tiempoActual = tiempoMaximo;
+        slider .maxValue = tiempoMaximo;
+        CambiarTemporizador(true);
+    }
+
+    public void DesactivarTemporizador()
+    {
+        CambiarTemporizador(false);
+    }
+}
